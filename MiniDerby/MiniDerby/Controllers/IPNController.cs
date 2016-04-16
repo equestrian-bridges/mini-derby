8edde7404a20322e45a6d19feac61e4635892919
jonathan.lucas <jonathan.lucas@LEN-W541-1252>
2016-04-16 14:26:46 -0500

    a

---
+++ MiniDerby/MiniDerby/Controllers/IPNController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using MiniDerby.Models;
+using MiniDerby.PayPal;
+
+namespace MiniDerby.Controllers
+{
+    public class IPNController : Controller
+    {
+        [HttpPost]
+        [AllowAnonymous]
+        public EmptyResult PayPalPaymentNotification(PayPalCheckoutInfo payPalCheckoutInfo)
+        {
+            PayPalListenerModel model = new PayPalListenerModel();
+            model.PayPalCheckoutInfo = payPalCheckoutInfo;
+            byte[] parameters = Request.BinaryRead(Request.ContentLength);
+
+            if (parameters != null)
+            {
+                model.GetStatus(parameters);
+            }
+
+            return new EmptyResult();
+        }
+    }
+}
+
