0847ec37ea3a68004ec85f56e1500b11dce7b870
rwillbrown <will@rwb.io>
2016-04-17 11:55:14 -0500

    Home page progress.

---
+++ MiniDerby/MiniDerby.Data/Entities/Horse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniDerby.Data
+{
+	public partial class Horse
+	{
+		public decimal TotalAmountRaised
+		{
+			get
+			{
+				return this.Donations.Sum(x => x.Amount).Value;
+			}
+		}
+	}
+}
