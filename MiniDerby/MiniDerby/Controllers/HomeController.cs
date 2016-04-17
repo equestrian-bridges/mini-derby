563dfb20396a5d45fb6f533a319b63aa08fae456
mike.cario <mike.cario@atlasdsr.com>
2016-04-17 09:24:04 -0500

    Contact Email

--- MiniDerby/MiniDerby/Controllers/HomeController.cs
+++ MiniDerby/MiniDerby/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using MiniDerby.Models.Home;
 using MiniDerby.Tools;
+using System.Net.Mail;
+using System.Text;
+using MiniDerby.Models.Contact;
 
 namespace MiniDerby.Controllers
 {
@@ -22,15 +25,52 @@ namespace MiniDerby.Controllers
 
 			return View();
 		}
-
+        
 		public ActionResult Contact()
 		{
 			ViewBag.Message = "Your contact page.";
 
 			return View();
 		}
+        
+        
+        [HttpPost]
+        public ActionResult Contact(ContactModel c)
+        {
+            try
+            {
+                var msg = new MailMessage();
+                var smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Credentials = new System.Net.NetworkCredential("fivenseven@gmail.com", "Moxyox12");
 
-		public ActionResult Horses()
+                var from = new MailAddress(c.Email.ToString());
+                var sb = new StringBuilder();
+
+                msg.To.Add("youremail@email.com");
+                msg.Subject = String.Format("Contact Us: {0}", c.Subject);
+                msg.IsBodyHtml = false;
+                smtp.Host = "mail.yourdomain.com";
+                sb.Append("Name: " + c.Name);
+                sb.Append(Environment.NewLine);
+                sb.Append("Email: " + c.Email);
+                sb.Append(Environment.NewLine);
+                sb.Append("Comments: " + c.Message);
+                msg.Body = sb.ToString();
+                smtp.Send(msg);
+                msg.Dispose();
+                return View("Success");
+            }
+            catch (Exception)
+            {
+                return View("Error");
+            }
+        }
+
+        public ActionResult Horses()
 		{
 			return View();
 		}
